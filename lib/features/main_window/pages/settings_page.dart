ed89d47eaae64accfb9d7a354bf8b48412b3bdaf
NeonMaliket <RoyalAnaconda@icloud.com>
2025-11-03 17:42:18 +0200

    add login page

--- lib/features/main_window/pages/settings_page.dart
+++ lib/features/main_window/pages/settings_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_settings_ui/flutter_settings_ui.dart';
+import 'package:go_router/go_router.dart';
+import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 import 'package:tiny/bloc/bloc.dart';
 import 'package:tiny/components/cyberpunk/cyberpunk.dart';
 import 'package:tiny/config/config.dart';
@@ -25,21 +27,12 @@ class SettingsPage extends StatelessWidget {
             style: context.theme().textTheme.titleMedium,
           ),
           tiles: [
-            CustomSettingsTile(
-              child: ListTile(
-                title: Text(
-                  'Clear Cache',
-                  style: context.theme().textTheme.titleSmall,
-                ),
-                shape: cyberpunkShape(
-                  cyberpunkBorderSide(
-                    context,
-                    color: context.theme().colorScheme.secondary,
-                  ).copyWith(width: 0.5),
-                ),
-                leading: Icon(CupertinoIcons.trash),
-                onTap: () async {
-                  await getIt<CacheRepository>().clearDocumentCache();
+            SettingsItem(
+              title: 'Clear Cache',
+              leading: Icon(CupertinoIcons.trash),
+              onTap: () async {
+                await getIt<CacheRepository>().clearDocumentCache();
+                if (context.mounted) {
                   cyberpunkAlertBloc.add(
                     ShowCyberpunkAlertEvent(
                       type: CyberpunkAlertType.success,
@@ -48,12 +41,58 @@ class SettingsPage extends StatelessWidget {
                           'The document cache has been cleared successfully.',
                     ),
                   );
-                },
+                }
+              },
+            ),
+            SettingsItem(
+              title: 'Logout',
+              leading: Icon(
+                CupertinoIcons.person_crop_circle_badge_xmark,
               ),
+              onTap: () async {
+                await getIt<CacheRepository>().clearDocumentCache();
+                await Supabase.instance.client.auth.signOut();
+                if (context.mounted) {
+                  context.go('/login');
+                }
+              },
             ),
           ],
         ),
       ],
+    );
+  }
+}
+
+class SettingsItem extends AbstractSettingsTile {
+  const SettingsItem({
+    super.key,
+    required this.title,
+    required this.onTap,
+    required this.leading,
+  });
+
+  final Widget? leading;
+  final String title;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomSettingsTile(
+      child: ListTile(
+        title: Text(
+          title,
+          style: context.theme().textTheme.titleSmall,
+        ),
+        shape: cyberpunkShape(
+          cyberpunkBorderSide(
+            context,
+            color: context.theme().colorScheme.secondary,
+          ).copyWith(width: 0.5),
+        ),
+        leading: leading,
+        onTap: onTap,
+      ),
     );
   }
 }
