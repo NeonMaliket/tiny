ee0d28d9719e188d5a297d1e95186c79ef018023
NeonMaliket <RoyalAnaconda@icloud.com>
2025-12-25 18:50:32 +0200

    Use PUBLIC_ env var names for Supabase

--- lib/config/supabase.dart
+++ lib/config/supabase.dart
@@ -1,8 +1,8 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-final supabaseUrl = dotenv.env['NEXT_PUBLIC_SUPABASE_URL'];
-final supabaseAnonKey = dotenv.env['NEXT_PUBLIC_SUPABASE_ANON_KEY'];
+final supabaseUrl = dotenv.env['PUBLIC_SUPABASE_URL'];
+final supabaseAnonKey = dotenv.env['PUBLIC_SUPABASE_ANON_KEY'];
 
 supabase() async {
   await Supabase.initialize(
